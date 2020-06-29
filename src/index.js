77ebdd32c7469e2abe238c1f032282f70b45cdbc
Israel Fagundes <israel.fagundes2001@gmail.com>
2020-06-29 12:25:58 -0300

    sequelize configured

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const db = require('./models');
 
 const app = express();
 
 app.get('/', (req, res) => {});
 
-app.listen(3001, () => {
-  console.log('Server running on port 3001');
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log('Server running on port 3001');
+  });
 });
+
