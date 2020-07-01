92a576408c5254eb8484fcc0408c9c481622a2e1
Israel Fagundes <israel.fagundes2001@gmail.com>
2020-07-01 14:24:54 -0300

    validation added

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,14 +2,16 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 
 const { Account } = require('../models'); 
+const { accountSignUp } = require('../validators/account');
+const { getMessages } = require('../helpers/messages');
 
 const router = express.Router();
 
 router.get('/sign-in', (req, res) => {
-  return res.json('Sign-in');
+  return res.jsonOK(null);
 })
 
-router.get('/sign-up', async (req, res) => {
+router.get('/sign-up', accountSignUp, async (req, res) => {
   const { email, password } = req.body;
 
   /**
@@ -21,14 +23,14 @@ router.get('/sign-up', async (req, res) => {
   })
 
   if (accountExists) {
-    return res.jsonBadRequest(null, 'Account already exists');
+    return res.jsonBadRequest(null, getMessages('account.signup.email.exists'));
   }
   
   const hash = bcrypt.hashSync(password, 10);
   
   const newAccount = await Account.create({ email, password: hash });
   
-  return res.jsonOK(newAccount, 'Account successfully created');
+  return res.jsonOK(newAccount, getMessages('account.signup.success'));
 });
 
 module.exports = router; 
