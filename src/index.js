be2f37fee5212f223bfb0f9009e55746e630d56c
Israel Fagundes <israel.fagundes2001@gmail.com>
2020-07-02 12:42:57 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const db = require('./models');
 const response = require('./middlewares/response');
@@ -9,6 +10,7 @@ const linkController = require('./controllers/link');
 
 const app = express();
 
+app.use(cors());
 app.use(response);
 app.use(checkJWT);
 
