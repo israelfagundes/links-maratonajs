b7c201bebe17cbdd1be71b5aa6786427e1e95251
Israel Fagundes <israel.fagundes2001@gmail.com>
2020-07-02 11:43:55 -0300

    excluded auth paths from jwt

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,11 +1,19 @@
 const { verifyJWT } = require('../helpers/jwt');
 
 const checkJWT = (req, res, next) => {
+  const { url: path } = req;
+
+  const excludedPaths = ['/auth/sign-in', '/auth/sign-up'];
+
+  const isExcluded = !!excludedPaths.find(p => p.startsWith(path));
+
+  if (isExcluded) return next();
+  
   let token = req.headers['authorization'];
   
   token = token ? token.slice(7, token.length) : null;
 
-  if (!token) return res.jsonUnauthorized();
+  if (!token) return res.jsonUnauthorized(null, 'Invalid token');
   
   try {
     const decoded = verifyJWT(token);
@@ -13,7 +21,7 @@ const checkJWT = (req, res, next) => {
   } catch (err) {
     return res.jsonUnauthorized(null, 'Invalid token');
   }
-  
+
   next();
 }
 
