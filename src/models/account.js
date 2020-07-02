dd60480879e77c27b0299035a96417022a0b3b1d
Israel Fagundes <israel.fagundes2001@gmail.com>
2020-07-02 11:12:35 -0300

    add links CRUD

--- src/models/account.js
+++ src/models/account.js
@@ -10,11 +10,15 @@ module.exports = (sequelize, DataTypes) => {
     },
   });
 
+  Account.associate = models => {
+    Account.hasMany(models.Link, { foreignKey: 'accountId' });
+  };
+
   Account.prototype.toJSON = function() {
     const values = { ...this.get() };
     delete values.password;
     return values;  
-  }
+  };
 
   return Account;
 }
