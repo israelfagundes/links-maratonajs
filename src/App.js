d5e379f07abb8a9d6153b164d6e1e3c2d5283e79
Israel <israel.fagundes2001@gmail.com>
2020-07-02 13:59:27 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 const App = () => {
-  return <h1>Link</h1>
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/sign-in">Sign in</Link>
+            </li>
+            <li>
+              <Link to="/sign-up">Sign up</Link>
+            </li>
+            <li>
+              <Link to="/manage/links/create">Create Link</Link>
+            </li>
+            <li>
+              <Link to="/manage/links/edit">Edit Link</Link>
+            </li>
+            <li>
+              <Link to="/manage/links">Links</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/sign-in">Sign In</Route>
+          <Route path="/sign-up">Sign Up</Route>
+          <Route path="/manage/links/create">Create Link</Route>
+          <Route path="/manage/links/edit">Edit Link</Route>
+          <Route path="/manage/links">Links</Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
