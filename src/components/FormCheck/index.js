3ab6853fe49b65bd51d163e0d9e8db846784027b
Israel <israel.fagundes2001@gmail.com>
2020-07-04 15:48:27 -0300

    update links

--- src/components/FormCheck/index.js
+++ src/components/FormCheck/index.js
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from 'react';
 
 function FormCheck({ data }) {
-  const [isChecked, setIsChecked] = useState(null);
+  const [isChecked, setIsChecked] = useState(false);
 
+  useEffect(() => {
+    const initialValue = data && data.checked ? data.checked : undefined;
+
+    if (initialValue !== undefined) setIsChecked(initialValue);
+
+  }, [data]);
+  
   const handleChange = e => {
     if (isChecked === e.target.checked) return;
-    setIsChecked(!!e.target.checked);
+    setIsChecked(e.target.checked);
   }
 
   return (
     <div className="form-group form-check">
       <label className="form-check-label">
-        <input type="checkbox" name="isSocial"checked={!!isChecked} onChange={handleChange} />
+        <input type="checkbox" name="isSocial"checked={isChecked} onChange={handleChange} />
         <span className="form-check-sign"></span>
         Is Social
       </label>
